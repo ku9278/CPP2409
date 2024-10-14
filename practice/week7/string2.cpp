34ce9aeba3c334d20b03ca09f5dbafae59ed82ea
ku9278 <ku9278@naver.com>
2024-10-14 16:00:36 +0900

    Function and String Exercises

---
+++ practice/week7/string2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s = "Hello World!";
+
+    s.erase(7, 2);
+    cout << "index 7부터 2까지 문자열 제거: " << s << endl;
+
+    int found = s.find("Hello");
+    if (found >= 0){
+        cout << "찾은 위치: " << found << endl;
+    }
+
+    found = s.find(".", 4);
+    if (found >= 0){
+        cout << "특정 위치 이후로 찾은 위치: " << found << endl;
+    }
+
+    found = s.find("World");
+    if (found >= 0){
+        cout << "찾은 위치: " << found << endl;
+    }
+    else{
+        cout << "찾을 수 없습니다: found = " << found << endl;
+    }
+
+    return 0;
+}
