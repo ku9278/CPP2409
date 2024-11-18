e112d7eeb0ecf50640b11c52ed785702ba6a76aa
ku9278 <ku9278@naver.com>
2024-11-18 16:12:50 +0900

    Operator Overloading & Friend Function Exercises

---
+++ project/week12_mud/user.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "user.h"
+
+User::User(){
+    hp = 20;
+}
+
+int User::getUser_x() { return user_x; }
+int User::getUser_y() { return user_y; }
+int User::getHp() { return hp; }
+
+void User::Move(int move_x, int move_y){
+    user_x += move_x;
+    user_y += move_y;
+}
+void User::DecreaseHP(int dec_hp){
+    hp -= dec_hp;
+}
+void User::IncreaseHP(int inc_hp){
+    hp += inc_hp;
+}
+void User::IncreaseItem(int inc_item){
+    itemCnt += inc_item;
+}
+
+ostream& operator << (ostream& os, const User& u){
+    os << "현재 HP는 " << u.hp << "이고, 먹은 아이템은 총 " << u.itemCnt << "개 입니다." << endl;
+    return os;
+}
---
+++ project/week12_mud/user.h
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class User{
+private:
+	// 유저의 위치를 저장할 변수
+	int user_x = 0; // 가로
+	int user_y = 0; // 세로
+	// 유저의 HP
+	int hp;
+	// 아이템 먹은 횟수
+	int itemCnt = 0;
+
+public:
+    User();
+
+	int getUser_x();
+	int getUser_y();
+	int getHp();
+
+	void Move(int move_x, int move_y);
+    void DecreaseHP(int dec_hp);
+    void IncreaseHP(int inc_hp);
+	void IncreaseItem(int inc_item);
+
+	friend ostream& operator << (ostream& os, const User& u);
+};
